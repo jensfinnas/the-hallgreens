8dbfae182e0edf96c2bec51d879d13f4d0385c04
jensfinnas <jens.finnas@gmail.com>
2016-06-29 17:03:01 +0200

    Add hearts

--- app/scripts/chart.js
+++ app/scripts/chart.js
@@ -94,11 +94,16 @@ function update(source) {
       .classed("no-partner", function(d) { return !d["hasPartner"]; })
       .attr("transform", "translate("+ [0, nodeSize] +")")
 
-    partnerGroup.append("circle")
+    /*partnerGroup.append("circle")
        .attr("x", - nodeSize / 2)
        .attr("y", (nodeSize / 2 + partnerNodeSize / 2) * 0.8)
        .attr("r", partnerNodeSize / 2)
-       .attr("fill", "red");
+       .attr("fill", "red");*/
+
+    partnerGroup.append("path")
+      .attr("class", "heart")
+      .attr("d","M23.6,0c-3.4,0-6.3,2.7-7.6,5.6C14.7,2.7,11.8,0,8.4,0C3.8,0,0,3.8,0,8.4c0,9.4,9.5,11.9,16,21.2c6.1-9.3,16-12.1,16-21.2C32,3.8,28.2,0,23.6,0z")
+      .attr("transform", "translate("+[- partnerNodeSize / 2,- partnerNodeSize / 2]+"), scale(0.3)");
 
     partnerGroup.append("text")
       .attr("x", function(d,i) { return d.depth == 0 ? -10 : 10; })
